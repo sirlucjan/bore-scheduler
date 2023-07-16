30cd62b22c40ca6d234a68bdea6bfb5ca63af9df
Masahito S <firelzrd@gmail.com>
2023-07-17 02:15:08 +0900

    Revert "sched_burst_penalty_offset default 12 -> 18"
    This reverts commit dba0d6994a40e90f2a909f29f183ab062d2f18cd.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -132,7 +132,7 @@ const_debug unsigned int sysctl_sched_migration_cost	= 500000UL;
 #ifdef CONFIG_SCHED_BORE
 unsigned int __read_mostly sched_bore                 = 1;
 unsigned int __read_mostly sched_burst_cache_lifetime = 60000000;
-unsigned int __read_mostly sched_burst_penalty_offset = 18;
+unsigned int __read_mostly sched_burst_penalty_offset = 12;
 unsigned int __read_mostly sched_burst_penalty_scale  = 1292;
 unsigned int __read_mostly sched_burst_smoothness     = 1;
 unsigned int __read_mostly sched_burst_fork_atavistic = 1;
